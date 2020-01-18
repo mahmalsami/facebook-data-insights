e079b83b189bc9817aeb658b486f5ddc774967e5
Samix <reaverhack@gmail.com>
2020-01-19 00:43:21 +0800

    add insights on location timestamp frequency

--- facebook.py
+++ facebook.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from pandas.io.json import json_normalize
 import json
+from datetime import datetime
 
 
 #1.ADS
@@ -21,15 +22,16 @@ print("Number of tracked ads topics :",len(adsInterests))
 
 
 # 2.Location
-# transfrom from JSON to GEOJSON for QGIS
-#extacting frequency
+# transfrom from JSON to GEOJSON for QGIS display purpose
+#extacting frequency of extraction
 frequency=[]
+timestampArray=[]
 rawJsonLocation = pd.read_json (r"location/location_history.json")
 GeoJSONLocation = {
   "type": "FeatureCollection",
   "features": []}
 i=0
-index=0
+
 for row in rawJsonLocation["location_history"]:
     #First I load the dict (one at a time)
 	locationPoint={}
@@ -45,18 +47,41 @@ for row in rawJsonLocation["location_history"]:
 	    "coordinates": [row['coordinate']['longitude'], row['coordinate']['latitude']],
 		}
 	}
+	#Timestamps seems unsorted, doing it manually
+	timestampArray.append(row['creation_timestamp'])
 	GeoJSONLocation["features"].append(locationPoint)
-	if index>0 and index<len(GeoJSONLocation["features"]):
-		diff=GeoJSONLocation["features"][index-1]["properties"]["timestamp"]-GeoJSONLocation["features"][index]["properties"]["timestamp"]
-		frequency.append(diff)
 
+#Extracting info around location's timestamps
+index=0
+timestampArray.sort()
+
+for row in timestampArray:
+	diff=timestampArray[index]
+	if index>1:
+		diff=(timestampArray[index]-timestampArray[index-1])
+	frequency.append(diff)
 	index=index+1
 
+
+#Extract location per timeslot (day, night)
+#TODO
+
+
+#Store all files
 with open('location/location_parsed.json', 'w') as outfile:
     json.dump(GeoJSONLocation, outfile)
+with open('location/frequency.json', 'w') as outfile:
+    json.dump(frequency, outfile)
+
 
 averageSeconds= sum(frequency) / float(len(frequency))
-print(averageSeconds/3600)
+print("First location logged",datetime.fromtimestamp(GeoJSONLocation["features"][len(GeoJSONLocation["features"])-1]["properties"]["timestamp"]))
+print("Last location logged",datetime.fromtimestamp(GeoJSONLocation["features"][0]["properties"]["timestamp"]))
+print("Average of extractions in days",averageSeconds/(3600*24))
+print("min frequency in seconds",min(frequency))
+print("max frequency in days:",max(frequency)/(3600*24))
+
+
 
 
 
